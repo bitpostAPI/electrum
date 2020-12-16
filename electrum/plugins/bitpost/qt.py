72eabd441de67bb6094cc1b6e19393adcd4f39bc
pedro-roberto <pedro.fr.22@gmail.com>
2020-12-16 02:23:39 +0000

    Add help messages and error messages in settings. Refactor settings.

--- electrum/plugins/bitpost/qt.py
+++ electrum/plugins/bitpost/qt.py
@@ -18,7 +18,7 @@ from electrum.util import NotEnoughFunds
                                   
 from .interface import BitpostInterface
 from datetime import datetime
-from .utils import get_fee_units
+from .utils import create_settings_window
                                   
                                   
 class Plugin(BasePlugin):
@@ -34,12 +34,6 @@ class Plugin(BasePlugin):
     def __init__(self, parent, config, name):
         BasePlugin.__init__(self, parent, config, name)
         self.wallets=set()
-        self.max_fee = self.config.get('bitpost_max_fee', self.default_max_fee)
-        self.max_fee_unit = self.config.get('bitpost_max_fee_unit', self.default_max_fee_unit)
-        self.num_txs = self.config.get('bitpost_num_txs', self.default_num_txs)
-        self.delay = self.config.get('bitpost_delay', self.default_delay)
-        self.target_interval = self.config.get('bitpost_target_interval', self.default_target_mins)
-
         
     def requires_settings(self):
         return True
@@ -51,164 +45,7 @@ class Plugin(BasePlugin):
 
 
     def settings_dialog(self, window):
-        # Return a settings dialog.
-        main_window = get_parent_main_window(window)
-
-        d = WindowModalDialog(window, _("Bitpost settings"))
-        vbox = QVBoxLayout(d)
-        
-        d.setMinimumSize(500, 200)
-
-        hbox_maxfee = QHBoxLayout()
-        hbox_maxfee.addWidget(QLabel("Default max fee to use"))
-        max_fees_e = QLineEdit()
-        max_fees_e.setText(str(self.max_fee))
-        hbox_maxfee.addWidget(max_fees_e)
-
-        fee_combo = QComboBox()
-        fee_combo_values = get_fee_units(main_window, self.max_fee_unit)
-
-        fee_combo.addItems(fee_combo_values)
-        hbox_maxfee.addWidget(fee_combo)
-
-        help_button__max_fee = QPushButton("?")
-        hbox_maxfee.addWidget(help_button__max_fee)
-
-        vbox.addLayout(hbox_maxfee)
-
-        empty_line = QHBoxLayout()
-        empty_line.addWidget(QLabel(""))
-        vbox.addLayout(empty_line)
-
-        advanced_settings_title = QHBoxLayout()
-        advanced_settings_title.addStretch()
-        advanced_settings_title.addWidget(QLabel("<b>Notifications</b>"))
-        advanced_settings_title.addStretch()
-        vbox.addLayout(advanced_settings_title)
-
-        platform_address = QHBoxLayout()
-
-        platform_address.addWidget(QLabel("Platform"))
-        platform_combo = QComboBox()
-        fee_combo_values = ['None', 'Email', 'Twitter']
-        platform_combo.addItems(fee_combo_values)
-        platform_combo.setCurrentText(self.window.config.get('bitpost_notification_platform'))
-        platform_address.addWidget(platform_combo)
-
-        platform_address.addWidget(QLabel("Address/handle"))
-        vbox.addLayout(platform_address)
-        address_input = QLineEdit()
-        address_input.setText(self.config.get('bitpost_notification_address', ''))
-        platform_address.addWidget(address_input)
-
-        subscription_title = QHBoxLayout()
-        subscription_title.addWidget(QLabel("Subscriptions"))
-        subscription_title.addWidget(QPushButton("?"))
-        subscription_title.addStretch()
-        vbox.addLayout(subscription_title)
-
-        subscriptions = {subscription['name'] for subscription in self.window.config.get('bitpost_notification_subscriptions')}
-        subscriptions1 = QVBoxLayout()
-        overdue_checkbox = QCheckBox("Overdue")
-        if 'overdue' in subscriptions:
-            overdue_checkbox.setChecked(True)
-        subscriptions1.addWidget(overdue_checkbox)
-
-        mined_checkbox = QCheckBox("Mined")
-        if 'mine' in subscriptions:
-            mined_checkbox.setChecked(True)
-        subscriptions1.addWidget(mined_checkbox)
-        max_fee_reached_checkbox = QCheckBox("Maximum fee reached")
-        if 'reached' in subscriptions:
-            max_fee_reached_checkbox.setChecked(True)
-        subscriptions1.addWidget(max_fee_reached_checkbox)
-        vbox.addLayout(subscriptions1)
-
-        reorg_checkbox = QCheckBox("Block reorg")
-        if 'orphaned_block' in subscriptions:
-            reorg_checkbox.setChecked(True)
-        subscriptions1.addWidget(reorg_checkbox)
-        orphaned_checkbox = QCheckBox("Child tx orphaned")
-        if '' in subscriptions:
-            orphaned_checkbox.setChecked(True)
-        subscriptions1.addWidget(orphaned_checkbox)
-
-        advanced_settings_title = QHBoxLayout()
-        advanced_settings_title.addStretch()
-        advanced_settings_title.addWidget(QLabel("<b>Advanced Settings</b>"))
-        advanced_settings_title.addStretch()
-        vbox.addLayout(advanced_settings_title)
-
-        hbox_ntx=QHBoxLayout()
-        hbox_ntx.addWidget(QLabel("Default number of Txs"))
-        num_txs_e=QLineEdit()
-        num_txs_e.setText(str(self.num_txs))
-        hbox_ntx.addWidget(num_txs_e)
-        help_button__num_txs = QPushButton("?")
-        hbox_ntx.addWidget(help_button__num_txs)
-        vbox.addLayout(hbox_ntx)
-
-        broadcast_policy = QHBoxLayout()
-        broadcast_policy.addWidget(QLabel("First broadcast policy"))
-
-        broadcast_policy_combo = QComboBox()
-        # 'Broadcast lowest fee transaction immediatly'
-        broadcast_policy_options = ['Don\'t delay first broadcast', 'Allow delay of first broadcast']
-        if self.delay == 1:
-            broadcast_policy_options.reverse()
-
-        broadcast_policy_combo.addItems(broadcast_policy_options)
-        broadcast_policy.addWidget(broadcast_policy_combo)
-
-        help_button__broadcast_policy = QPushButton("?")
-        broadcast_policy.addWidget(help_button__broadcast_policy)
-        vbox.addLayout(broadcast_policy)
-
-        # hbox_target_interval = QHBoxLayout()
-        # hbox_target_interval.addWidget(QLabel("Target interval"))
-        # target_interval_e=QLineEdit()
-        # target_interval_e.setText(str(self.target_interval))
-        # hbox_target_interval.addWidget(target_interval_e)
-        # vbox.addLayout(hbox_target_interval)
-
-        vbox.addStretch()
-        vbox.addLayout(Buttons(CloseButton(d), OkButton(d)))
-
-        if not d.exec_():
-            return
-            
-        self.max_fee = str(max_fees_e.text())
-        self.config.set_key('bitpost_max_fee', self.max_fee)
-
-        self.config.set_key('bitpost_max_fee_unit', fee_combo.currentText())
-        self.max_fee_unit = fee_combo.currentText()
-
-        num_txs = str(num_txs_e.text())
-        self.config.set_key('bitpost_num_txs', num_txs)
-        self.num_txs = num_txs
-
-        self.delay = 1 if broadcast_policy_combo.currentText() == 'Allow delay of first broadcast' else 0
-        self.config.set_key('bitpost_delay', self.delay)
-
-        if not self.valid_address(platform_combo.currentText(), address_input.text()):
-            pass # TODO error window
-
-        self.config.set_key('bitpost_notification_platform', platform_combo.currentText())
-        self.config.set_key('bitpost_notification_address', address_input.text())
-
-        subscriptions = []
-        if overdue_checkbox.isChecked():
-            subscriptions.append({'name': 'overdue'})
-        if mined_checkbox.isChecked():
-            subscriptions.append({'name': 'mine'})
-        if max_fee_reached_checkbox.isChecked():
-            subscriptions.append({'name': 'reached'})
-        if reorg_checkbox.isChecked():
-            subscriptions.append({'name': 'orphaned_block'})
-        if orphaned_checkbox.isChecked():
-            pass # TODO
-
-        self.config.set_key('bitpost_notification_subscriptions', subscriptions)
+        create_settings_window(window)
 
     def valid_address(self, platform, address):
         return True  # TODO validation
@@ -231,19 +68,6 @@ class Plugin(BasePlugin):
         except:
             self.window.show_error(_("Not enought funds, please add more inputs or reduce max fee"))
             raise NotEnoughFunds
-            """
-            print(str(new_fee) + "bump fee method 2")
-            tx_out = self.window.wallet._bump_fee_through_decreasing_outputs(
-                tx=tx, new_fee_rate=new_fee)
-               
-        new_inputs=tx_out.inputs()
-        my_dict = {i:new_inputs.count(i) for i in new_inputs}
-        print(my_dict)
-        for k in my_dict:
-            if my_dict[k] > 1:
-                print(f"DUPLICATED INPUT {k}:{v}")
-                raise Exception(f"DUPLICATED INPUT {k}:{v}")
-        """
         return tx_out
 
         
@@ -327,7 +151,7 @@ class Plugin(BasePlugin):
             try:
                 delay = delay.timestamp()
             except:
-                delay = self.delay
+                delay = self.config.get('bitpost_delay')
                 
             now=datetime.now().timestamp()
             if target < delay:
@@ -347,10 +171,10 @@ class Plugin(BasePlugin):
             request = bitpost_interface.create_bitpost_request(raw_signed_txs, 
                                                 int(target), delay=int(delay))
 
-            if self.window.config.get('bitpost_notification_platform') != 'None':
-                request.add_notification(self.window.config.get('bitpost_notification_platform'),
-                                         self.window.config.get('bitpost_notification_address'),
-                                         self.window.config.get('bitpost_notification_subscriptions'))
+            if self.config.get('bitpost_notification_platform') != 'None':
+                request.add_notification(self.config.get('bitpost_notification_platform'),
+                                         self.config.get('bitpost_notification_address'),
+                                         self.config.get('bitpost_notification_subscriptions'))
 
             response=request.send_request().json()
             if response['status'] == 'success':
@@ -371,18 +195,25 @@ class Plugin(BasePlugin):
         self.wallet=wallet
         self.window = main_window
 
-        self.config.set_key('bitpost_max_fee', self.max_fee)
-        self.config.set_key('bitpost_max_fee_unit', self.max_fee_unit)
+        max_fee = self.config.get('bitpost_max_fee', self.default_max_fee)
+        self.config.set_key('bitpost_max_fee', max_fee)
 
-        self.config.set_key('bitpost_num_txs', self.num_txs)
-        self.config.set_key('bitpost_delay', self.delay)
+        max_fee_unit = self.config.get('bitpost_max_fee_unit', self.default_max_fee_unit)
+        self.config.set_key('bitpost_max_fee_unit', max_fee_unit)
 
-        self.config.set_key('bitpost_target_interval', self.target_interval)
-        self.config.set_key('bitpost_notification_platform', self.config.get('bitpost_notification_platform'),
-                            self.default_notification_platform)
+        num_txs = self.config.get('bitpost_num_txs', self.default_num_txs)
+        self.config.set_key('bitpost_num_txs', num_txs)
 
-        self.config.set_key('bitpost_notification_address', self.config.get('bitpost_notification_address'), '')
-        self.config.set_key('bitpost_notification_subscriptions', self.config.get('bitpost_notification_subscriptions'), [])
+        delay = self.config.get('bitpost_delay', self.default_delay)
+        self.config.set_key('bitpost_delay', delay)
+
+        target_interval = self.config.get('bitpost_target_interval', self.default_target_mins)
+        self.config.set_key('bitpost_target_interval', target_interval)
+
+        self.config.set_key('bitpost_notification_platform', self.config.get('bitpost_notification_platform',
+                            self.default_notification_platform))
+        self.config.set_key('bitpost_notification_address', self.config.get('bitpost_notification_address', ''))
+        self.config.set_key('bitpost_notification_subscriptions', self.config.get('bitpost_notification_subscriptions', []))
 
     @hook
     def close_wallet(self, wallet):
@@ -392,16 +223,13 @@ class Plugin(BasePlugin):
     def close_settings_dialog(self):
         has_fx = self.window.fx and self.window.fx.is_enabled()
 
-        has_different_fx = self.max_fee_unit != 'sats' and  \
-            self.max_fee_unit != 'sats/byte' and \
-            self.window.fx.get_currency() != self.max_fee_unit
+        has_different_fx = self.config.get('bitpost_max_fee_unit') != 'sats' and  \
+            self.config.get('bitpost_max_fee_unit') != 'sats/byte' and \
+            self.window.fx.get_currency() != self.config.get('bitpost_max_fee_unit')
             
         if has_fx and has_different_fx:
-            self.max_fee_unit = self.default_max_fee_unit
-            self.config.set_key('bitpost_max_fee_unit', self.max_fee_unit)
-
-            self.max_fee = self.default_max_fee
-            self.config.set_key('bitpost_max_fee', self.max_fee)
+            self.config.set_key('bitpost_max_fee_unit', self.default_max_fee_unit)
+            self.config.set_key('bitpost_max_fee', self.default_max_fee)
 
     @hook
     def create_send_tab(self, grid):
--- electrum/plugins/bitpost/utils.py
+++ electrum/plugins/bitpost/utils.py
@@ -1,3 +1,10 @@
+from electrum.gui.qt.util import (EnterButton, Buttons, CloseButton, OkButton, WindowModalDialog, get_parent_main_window)
+from electrum.gui.qt.main_window import ElectrumWindow
+from PyQt5.QtWidgets import (QVBoxLayout, QHBoxLayout, QLabel, QInputDialog, QPushButton, QCheckBox,QLineEdit, QComboBox)
+from functools import partial
+from electrum.i18n import _
+
+
 def get_fee_units(main_window, default):
     fee_combo_values = ['sats', 'sats/byte']
     if main_window.fx and main_window.fx.is_enabled():
@@ -6,3 +13,197 @@ def get_fee_units(main_window, default):
         fee_combo_values.remove(default)
         fee_combo_values.insert(0, default)
     return fee_combo_values
+
+
+class HelpTexts:
+
+    max_fee = 'The maximum amount you are willing to pay for a bitcoin transaction. ' \
+              'The actual fee you will pay will likely be lower than the amount you choose, especially' \
+              'if you choose a high value. You can choose a maximum in fiat if you enabled a fiat currency' \
+              'under Tools > Preferences > Fiat'
+
+    subscriptions = 'You can subscribe to notifications about important events regarding your payments.' \
+                    'Here is a explanation of the avaible events/subscriptions:\n' \
+                    '- Overdue: the payment hasn\'t been confirmed before the chosen deadline.\n' \
+                    '- Mined: the payment has been mined/confirmed.\n' \
+                    '- Maximum fee reached: the transaction with the maximum fee has been broadcasted....\n' \
+                    '...'
+
+    num_txs = 'The number of transactions that will be broadcasted by default to bitpost.'
+    delay = 'When you schedule a payment with a deadline far into the future, bitpost may choose to broadcast the' \
+            'first transaction posteriorly. You may allow or disallow this practice.'
+
+
+def is_valid_settings(max_fee_input):
+    try:
+        int(max_fee_input)
+        return True
+    except:
+        return False
+
+
+def create_settings_window(small_window):
+    window = get_parent_main_window(small_window)
+
+    d = WindowModalDialog(window, _("Bitpost settings"))
+    vbox = QVBoxLayout(d)
+
+    d.setMinimumSize(500, 200)
+
+    hbox_maxfee = QHBoxLayout()
+    hbox_maxfee.addWidget(QLabel("Default max fee to use"))
+    max_fees_e = QLineEdit()
+    max_fees_e.setText(str(window.config.get('bitpost_max_fee')))
+    hbox_maxfee.addWidget(max_fees_e)
+
+    fee_combo = QComboBox()
+    fee_combo_values = get_fee_units(window, window.config.get('bitpost_max_fee_unit'))
+
+    fee_combo.addItems(fee_combo_values)
+    hbox_maxfee.addWidget(fee_combo)
+
+    help_button__max_fee = QPushButton("?")
+    help_button__max_fee.clicked.connect(lambda: d.show_message(HelpTexts.max_fee))
+    hbox_maxfee.addWidget(help_button__max_fee)
+
+    vbox.addLayout(hbox_maxfee)
+
+    empty_line = QHBoxLayout()
+    empty_line.addWidget(QLabel(""))
+    vbox.addLayout(empty_line)
+
+    advanced_settings_title = QHBoxLayout()
+    advanced_settings_title.addStretch()
+    advanced_settings_title.addWidget(QLabel("<b>Notifications</b>"))
+    advanced_settings_title.addStretch()
+    vbox.addLayout(advanced_settings_title)
+
+    platform_address = QHBoxLayout()
+
+    platform_address.addWidget(QLabel("Platform"))
+    platform_combo = QComboBox()
+    fee_combo_values = ['None', 'Email', 'Twitter']
+    platform_combo.addItems(fee_combo_values)
+    platform_combo.setCurrentText(window.config.get('bitpost_notification_platform'))
+    platform_address.addWidget(platform_combo)
+
+    platform_address.addWidget(QLabel("Address/handle"))
+    vbox.addLayout(platform_address)
+    address_input = QLineEdit()
+    address_input.setText(window.config.get('bitpost_notification_address', ''))
+    platform_address.addWidget(address_input)
+
+    subscription_title = QHBoxLayout()
+    subscription_title.addWidget(QLabel("Subscriptions"))
+    subscriptions_help = QPushButton("?")
+    subscriptions_help.clicked.connect(lambda: d.show_message(HelpTexts.subscriptions))
+    subscription_title.addWidget(subscriptions_help)
+    subscription_title.addStretch()
+    vbox.addLayout(subscription_title)
+
+    subscriptions = {subscription['name'] for subscription in
+                     window.config.get('bitpost_notification_subscriptions')}
+    subscriptions1 = QVBoxLayout()
+    overdue_checkbox = QCheckBox("Overdue")
+    if 'overdue' in subscriptions:
+        overdue_checkbox.setChecked(True)
+    subscriptions1.addWidget(overdue_checkbox)
+
+    mined_checkbox = QCheckBox("Mined")
+    if 'mine' in subscriptions:
+        mined_checkbox.setChecked(True)
+    subscriptions1.addWidget(mined_checkbox)
+    max_fee_reached_checkbox = QCheckBox("Maximum fee reached")
+    if 'reached' in subscriptions:
+        max_fee_reached_checkbox.setChecked(True)
+    subscriptions1.addWidget(max_fee_reached_checkbox)
+    vbox.addLayout(subscriptions1)
+
+    reorg_checkbox = QCheckBox("Block reorg")
+    if 'orphaned_block' in subscriptions:
+        reorg_checkbox.setChecked(True)
+    subscriptions1.addWidget(reorg_checkbox)
+    orphaned_checkbox = QCheckBox("Child tx orphaned")
+    if '' in subscriptions:
+        orphaned_checkbox.setChecked(True)
+    subscriptions1.addWidget(orphaned_checkbox)
+
+    advanced_settings_title = QHBoxLayout()
+    advanced_settings_title.addStretch()
+    advanced_settings_title.addWidget(QLabel("<b>Advanced Settings</b>"))
+    advanced_settings_title.addStretch()
+    vbox.addLayout(advanced_settings_title)
+
+    hbox_ntx = QHBoxLayout()
+    hbox_ntx.addWidget(QLabel("Default number of Txs"))
+    num_txs_e = QLineEdit()
+    num_txs_e.setText(str(window.config.get('bitpost_num_txs')))
+    hbox_ntx.addWidget(num_txs_e)
+    help_button__num_txs = QPushButton("?")
+    help_button__num_txs.clicked.connect(lambda: d.show_message(HelpTexts.num_txs))
+    hbox_ntx.addWidget(help_button__num_txs)
+    vbox.addLayout(hbox_ntx)
+
+    broadcast_policy = QHBoxLayout()
+    broadcast_policy.addWidget(QLabel("First broadcast policy"))
+
+    broadcast_policy_combo = QComboBox()
+    # 'Broadcast lowest fee transaction immediatly'
+    broadcast_policy_options = ['Don\'t delay first broadcast', 'Allow delay of first broadcast']
+    if window.config.get('bitpost_delay') == 1:
+        broadcast_policy_options.reverse()
+
+    broadcast_policy_combo.addItems(broadcast_policy_options)
+    broadcast_policy.addWidget(broadcast_policy_combo)
+
+    help_button__broadcast_policy = QPushButton("?")
+    help_button__broadcast_policy.clicked.connect(lambda: d.show_message(HelpTexts.delay))
+    broadcast_policy.addWidget(help_button__broadcast_policy)
+    vbox.addLayout(broadcast_policy)
+
+    vbox.addStretch()
+    vbox.addLayout(Buttons(CloseButton(d), OkButton(d)))
+
+    if not d.exec_():
+        return
+
+    window.config.set_key('bitpost_max_fee_unit', fee_combo.currentText())
+    delay = 1 if broadcast_policy_combo.currentText() == 'Allow delay of first broadcast' else 0
+    window.config.set_key('bitpost_delay', delay)
+    window.config.set_key('bitpost_notification_platform', platform_combo.currentText())
+
+    subscriptions = []
+    if overdue_checkbox.isChecked():
+        subscriptions.append({'name': 'overdue'})
+    if mined_checkbox.isChecked():
+        subscriptions.append({'name': 'mine'})
+    if max_fee_reached_checkbox.isChecked():
+        subscriptions.append({'name': 'reached'})
+    if reorg_checkbox.isChecked():
+        subscriptions.append({'name': 'orphaned_block'})
+    if orphaned_checkbox.isChecked():
+        pass  # TODO
+    window.config.set_key('bitpost_notification_subscriptions', subscriptions)
+
+    try:
+        window.config.set_key('bitpost_max_fee', float(max_fees_e.text()))
+    except:
+        d.show_error('Invalid maximum fee, must be a number.')
+        create_settings_window(small_window)
+
+    try:
+        window.config.set_key('bitpost_num_txs', int(num_txs_e.text()))
+    except:
+        d.show_error('Invalid default number of transactions, must be an integer')
+        create_settings_window(small_window)
+
+    if not valid_address(platform_combo.currentText(), address_input.text()):
+        d.show_error('Invalid handle/address for the given platform')
+        create_settings_window(small_window)
+
+
+    window.config.set_key('bitpost_notification_address', address_input.text())
+
+
+def valid_address(platform, address):
+    return 3 < len(address) < 100  # TODO
